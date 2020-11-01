ad00da08ccbf330e9df85f894babe1ff1b928191
Muflih Kholidin <muflic.24@gmail.com>
2020-11-01 20:06:27 +0700

    Add paginator for raw query

--- paginator.go
+++ paginator.go
@@ -1,6 +1,9 @@
 package paginator
 
 import (
+	"fmt"
+	"strings"
+
 	"gorm.io/gorm"
 )
 
@@ -13,12 +16,12 @@ import (
 
 func gormPaginate(page, size int) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
-		if page == 0 {
+		if page <= 0 {
 			page = 1
 		}
 
 		if size <= 0 {
-			size = 10
+			size = 100
 		}
 
 		offset := (page - 1) * size
@@ -47,23 +50,18 @@ type Paginator struct {
 }
 
 type Paginate struct {
-	Page  int `json:"page"`
-	Size  int `json:"size"`
-	Total int `json:"total"`
+	Page  int   `json:"page"`
+	Size  int   `json:"size"`
+	Total int64 `json:"total"`
 }
 
-func Make(p *Config, gormDS interface{}) *Paginator {
+func Make(p *Config, ds interface{}) *Paginator {
 	db := p.DB
 
 	if p.ShowSQL {
 		db = db.Debug()
 	}
-	if p.Page < 1 {
-		p.Page = 1
-	}
-	if p.Size == 0 {
-		p.Size = 10
-	}
+
 	if len(p.OrderBy) > 0 {
 		for _, o := range p.OrderBy {
 			db = db.Order(o)
@@ -77,14 +75,65 @@ func Make(p *Config, gormDS interface{}) *Paginator {
 		db.Where(filter.Field+" "+filter.Operation+" ?", filter.Value)
 	}
 
-	db.Model(gormDS).Count(&count)
-	db.Scopes(gormPaginate(p.Page, p.Size)).Find(gormDS)
+	db.Model(ds).Count(&count)
+	db.Scopes(gormPaginate(p.Page, p.Size)).Find(ds)
 
-	result.Data = gormDS
+	result.Data = ds
 	result.Paginate = Paginate{
 		p.Page,
 		p.Size,
-		int(count),
+		count,
+	}
+
+	return &result
+}
+
+func MakeRaw(query string, p *Config, ds interface{}) *Paginator {
+
+	var result Paginator
+	var count int64
+
+	if p.Page <= 0 {
+		p.Page = 1
+	}
+
+	if p.Size <= 0 {
+		p.Size = 100
+	}
+
+	var where string
+
+	for id, filter := range p.Filters {
+		if id == 0 {
+			where += " where " + filter.Field + " " + filter.Operation + " " + filter.Value
+			continue
+		}
+		where += "	and " + filter.Field + " " + filter.Operation + " " + filter.Value
+	}
+	query += where
+	limitOffset := fmt.Sprintf(" limit %d offset %d ", p.Size, (p.Page-1)*p.Size)
+
+	order := ""
+	if len(p.OrderBy) > 0 {
+		order += " order by "
+		for _, o := range p.OrderBy {
+			order += o + ","
+		}
+		order = order[:len(order)-1]
+	}
+
+	err := p.DB.Raw(query + order + limitOffset).Scan(ds).Error
+	if err != nil {
+		fmt.Println("ERROR Paginator RAW", err)
+	}
+	queries := strings.Split(query, "from")
+	p.DB.Raw("select count(*) FROM " + queries[1]).Scan(&count)
+
+	result.Data = ds
+	result.Paginate = Paginate{
+		p.Page,
+		p.Size,
+		count,
 	}
 
 	return &result
